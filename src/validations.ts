27fb19c28c95f5059427c21f8f422c0e29f5ef18
neryjoao <joaonery1@gmail.com>
2024-12-31 13:02:19 +0100

    - refined the wateringScheduleSchema validation

--- src/validations.ts
+++ src/validations.ts
@@ -11,4 +11,21 @@ export const wateringScheduleSchema = z.object({
             dayOfTheWeek: z.enum(DAYS_OF_THE_WEEK).optional(),
             timeOfTheDay: z.string().time(),
             timeWateringInSec: z.number()}))  
+}).superRefine((arg, ctx) => {
+    const {regularWatering, repetitionPerDay, timeWateringInSec, customSchedule} = arg;
+    if (regularWatering && (!repetitionPerDay || !timeWateringInSec)) {
+        ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: `Needs repetitionPerDay and timeWateringInSec if regularWatering is set to true`
+        });
+    }
+
+    customSchedule.forEach(schedule => {
+        if (!schedule.everyDay && !schedule.dayOfTheWeek) {
+            ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: `If it is not watering every day, then it is necessary a week day`
+            })
+        }
+    });
 });
